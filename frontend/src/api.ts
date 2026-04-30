77a59f3f8c3fa7066d2e3c62a152a6a1194d28d4
Shubham kumar <kumarshubham33030@gmail.com>
2026-05-01 00:39:01 +0530

    corrected:env

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'BACKEND_URL', 
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000',
 });
 
 api.interceptors.request.use((config) => {
